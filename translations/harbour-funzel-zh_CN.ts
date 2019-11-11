23fb9a73cf3c824411c7e78c1984d4f012c501aa
dashinfantry <45334556+dashinfantry@users.noreply.github.com>
2019-11-11 11:15:58 +0800

    Create harbour-funzel-zh_CN.ts

---
+++ translations/harbour-funzel-zh_CN.ts
@@ -0,0 +1,189 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>About Funzel</source>
+        <translation>关于 Funzel</translation>
+    </message>
+    <message>
+        <source>A control center for the LEDs of the Gemini PDA</source>
+        <translation>一个用以控制 Gemini PDA LED 通知灯显示方式的软件</translation>
+    </message>
+    <message>
+        <source>By Sebastian J. Wolf</source>
+        <translation>作者为 Sebasitian J.Wolf</translation>
+    </message>
+    <message>
+        <source>Send E-Mail</source>
+        <translation>发送电子邮件</translation>
+    </message>
+    <message>
+        <source>Licensed under GNU GPLv3</source>
+        <translation>在 GNU GPLv3 许可协议下发布</translation>
+    </message>
+    <message>
+        <source>Sources on GitHub</source>
+        <translation>源代码位于 GitHub</translation>
+    </message>
+    <message>
+        <source>This product is distributed by &lt;a href=&quot;https://werkwolf.eu&quot;&gt;WerkWolf OÜ&lt;/a&gt;.</source>
+        <translation>该产品在以下网址发布 &lt;a href=&quot;https://werkwolf.eu&quot;&gt;WerkWolf OÜ&lt;/a&gt; 。</translation>
+    </message>
+</context>
+<context>
+    <name>AssignedContactsListItem</name>
+    <message>
+        <source>Remove Contact Assignment</source>
+        <translation>移除联系人 LED 通知灯色配置</translation>
+    </message>
+</context>
+<context>
+    <name>ColorSelectionPage</name>
+    <message>
+        <source>Select a Color</source>
+        <translation>选择灯色</translation>
+    </message>
+    <message>
+        <source>Red</source>
+        <translation>红色</translation>
+    </message>
+    <message>
+        <source>Green</source>
+        <translation>绿色</translation>
+    </message>
+    <message>
+        <source>Blue</source>
+        <translation>蓝色</translation>
+    </message>
+    <message>
+        <source>Yellow</source>
+        <translation>黄色</translation>
+    </message>
+    <message>
+        <source>Light Blue</source>
+        <translation>浅蓝色</translation>
+    </message>
+    <message>
+        <source>Purple</source>
+        <translation>紫色</translation>
+    </message>
+    <message>
+        <source>White</source>
+        <translation>白色</translation>
+    </message>
+    <message>
+        <source>Rainbow</source>
+        <translation>彩虹色</translation>
+    </message>
+</context>
+<context>
+    <name>ContactsSelectionPage</name>
+    <message>
+        <source>Refresh</source>
+        <translation>刷新</translation>
+    </message>
+    <message>
+        <source>Select a Contact</source>
+        <translation>选择联系人</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>On</source>
+        <translation>开启</translation>
+    </message>
+    <message>
+        <source>Off</source>
+        <translation>关闭</translation>
+    </message>
+</context>
+<context>
+    <name>LoadingIndicator</name>
+    <message>
+        <source>Loading...</source>
+        <translation>正在加载……</translation>
+    </message>
+</context>
+<context>
+    <name>OverviewPage</name>
+    <message>
+        <source>About Funzel</source>
+        <translation>关于 Funzel</translation>
+    </message>
+    <message>
+        <source>Welcome to Funzel</source>
+        <translation>欢迎使用 Funzel</translation>
+    </message>
+    <message>
+        <source>Enable LED animation on incoming call</source>
+        <translation>来电时显示 LED灯光通知</translation>
+    </message>
+    <message>
+        <source>When your Gemini PDA receives a call, the backside LEDs will show an animation.</source>
+        <translation>当你的 Gemini PDA 收到来电时，背部 LED 通知灯会以不同灯色进行提示。</translation>
+    </message>
+    <message>
+        <source>Red</source>
+        <translation>红色</translation>
+    </message>
+    <message>
+        <source>Green</source>
+        <translation>绿色</translation>
+    </message>
+    <message>
+        <source>Blue</source>
+        <translation>蓝色</translation>
+    </message>
+    <message>
+        <source>Yellow</source>
+        <translation>黄色</translation>
+    </message>
+    <message>
+        <source>Light Blue</source>
+        <translation>浅蓝色</translation>
+    </message>
+    <message>
+        <source>Purple</source>
+        <translation>紫色</translation>
+    </message>
+    <message>
+        <source>White</source>
+        <translation>白色</translation>
+    </message>
+    <message>
+        <source>Test Animation</source>
+        <translation>测试灯光提示</translation>
+    </message>
+    <message>
+        <source>No Gemini PDA found!</source>
+        <translation>没有找到 Gemini PDA !</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation>确认</translation>
+    </message>
+    <message>
+        <source>It seems that Funzel is not running on a Gemini PDA. As this is the only device which Funzel is supporting, this application will be rather useless on your device.</source>
+        <translation>似乎 Funzel 没有运行在 Gemini PDA 上。这是Funzel 唯一支持的设备。本软件在你的设备上毫无用处。</translation>
+    </message>
+    <message>
+        <source>Default Animation Color</source>
+        <translation>默认动画颜色</translation>
+    </message>
+    <message>
+        <source>Contact-specific Animation Color</source>
+        <translation>特定联系人 LED 通知灯色</translation>
+    </message>
+    <message>
+        <source>Assign Color to Contact</source>
+        <translation>配置不同联系人使用的灯色</translation>
+    </message>
+    <message>
+        <source>Rainbow</source>
+        <translation>彩虹色</translation>
+    </message>
+</context>
+</TS>
